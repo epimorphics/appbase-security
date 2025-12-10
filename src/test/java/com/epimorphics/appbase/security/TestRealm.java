423ba456ebaa1dffc5a95144784036bd9d7fa97d
Kal Ahmed <kal.ahmed@epimorphics.com>
2025-12-10 15:52:27 +0000

    Move tests to Jupiter API

--- src/test/java/com/epimorphics/appbase/security/TestRealm.java
+++ src/test/java/com/epimorphics/appbase/security/TestRealm.java
@@ -9,18 +9,16 @@
 
 package com.epimorphics.appbase.security;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.env.BasicIniEnvironment;
 import org.apache.shiro.env.Environment;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
-import org.apache.shiro.util.Factory;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class TestRealm {
     
